f6d69d81461a968725948bfd56ee3bf7b6daf2b6
Samrat Singh <samrat@MacBook-Pro.local>
2021-02-24 13:08:14 +0530

    solved

---
+++ divisor-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean divisorGame(int N) {
+        if(N%2 == 0)
+            return true;
+        return false;
+    }
+}
+
+
+// 1 2 3 4 5 6 7 8 9 10
+// F P F P F P F P F P
